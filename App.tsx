89b3d1aa42e0bd13bd8b100ab099fd5ca474db68
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2025-11-27 11:45:19 +1100

    remove auth, add in new supabase bits

--- App.tsx
+++ App.tsx
@@ -2,15 +2,15 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import {
-  StyleSheet,
-  Keyboard,
-  TouchableWithoutFeedback,
-  Pressable,
-  Text,
-  KeyboardAvoidingView,
-  Platform,
-  ScrollView,
-  View,
+	StyleSheet,
+	Keyboard,
+	TouchableWithoutFeedback,
+	Pressable,
+	Text,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	View,
 } from "react-native";
 
 // Components
@@ -21,145 +21,143 @@ import { useEffect, useState } from "react";
 
 // Constants
 import { CURRENT_DATE, CURRENT_TIME } from "./constants";
-import Auth from "./components/Auth";
 
 export default function App() {
-  // State
-  const [distance, setDistance] = useState<string>("");
-  const [incline, setIncline] = useState<string>("");
-  const [elevation, setElevation] = useState<string>("");
-  const [duration, setDuration] = useState<string>("");
-  const [date, setDate] = useState<string>(CURRENT_DATE);
-  const [time, setTime] = useState<string>(CURRENT_TIME);
-  const [title, setTitle] = useState<string>("");
+	// State
+	const [distance, setDistance] = useState<string>("");
+	const [incline, setIncline] = useState<string>("");
+	const [elevation, setElevation] = useState<string>("");
+	const [duration, setDuration] = useState<string>("");
+	const [date, setDate] = useState<string>(CURRENT_DATE);
+	const [time, setTime] = useState<string>(CURRENT_TIME);
+	const [title, setTitle] = useState<string>("");
 
-  // Calculate metres climbed when distance or incline changes
-  useEffect(() => {
-    if (distance && incline) {
-      const elevationGain = Number(distance) * Number(incline) * 10; // Convert km and % to meters
-      setElevation(elevationGain.toString());
-    }
-  }, [distance, incline]);
+	// Calculate metres climbed when distance or incline changes
+	useEffect(() => {
+		if (distance && incline) {
+			const elevationGain = Number(distance) * Number(incline) * 10; // Convert km and % to meters
+			setElevation(elevationGain.toString());
+		}
+	}, [distance, incline]);
 
-  const reset = () => {
-    setDistance("");
-    setIncline("");
-    setElevation("");
-    setDuration("");
-    setDate(CURRENT_DATE);
-    setTime(CURRENT_TIME);
-    setTitle("");
-  };
+	const reset = () => {
+		setDistance("");
+		setIncline("");
+		setElevation("");
+		setDuration("");
+		setDate(CURRENT_DATE);
+		setTime(CURRENT_TIME);
+		setTitle("");
+	};
 
-  const uploadRunToStrava = () => {
-    // Fetch access token
-    // Fetch athlete details
-    // Fetch activity details
-    // Upload activity
-  };
+	const uploadRunToStrava = () => {
+		// Fetch access token
+		// Fetch athlete details
+		// Fetch activity details
+		// Upload activity
+	};
 
-  return (
-    <SafeAreaView style={{ backgroundColor: "#000", flex: 1 }}>
-      <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-          <KeyboardAvoidingView
-            keyboardVerticalOffset={Platform.OS === "android" ? -500 : 0}
-          >
-            <View style={styles.titleContainer}>
-              <View style={{ flex: 1 }} />
-              <Text style={[styles.title, { flex: 1 }]}>Climbd</Text>
-              <Auth />
-            </View>
+	return (
+		<SafeAreaView style={{ backgroundColor: "#000", flex: 1 }}>
+			<View style={styles.container}>
+				<TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+					<KeyboardAvoidingView
+						keyboardVerticalOffset={Platform.OS === "android" ? -500 : 0}
+					>
+						<View style={styles.titleContainer}>
+							<View style={{ flex: 1 }} />
+							<Text style={[styles.title, { flex: 1 }]}>Climbd</Text>
+						</View>
 
-            <ScrollView
-              contentContainerStyle={styles.container}
-              scrollEnabled={false}
-            >
-              <View style={styles.inputContainer}>
-                <Input
-                  placeholder="Title (e.g. My Morning Run)"
-                  value={title}
-                  onChange={setTitle}
-                />
-                <Input
-                  placeholder="Distance (km)"
-                  value={distance}
-                  onChange={setDistance}
-                />
-                <Input
-                  placeholder="Duration (hh:mm:ss)"
-                  value={duration}
-                  onChange={setDuration}
-                />
-                <Input
-                  placeholder="Date (dd/mm/yyyy)"
-                  value={date}
-                  onChange={setDate}
-                />
-                <Input
-                  placeholder="Time (hh:mm)"
-                  value={time}
-                  onChange={setTime}
-                />
-                <Input
-                  placeholder="Incline (%)"
-                  value={incline}
-                  onChange={setIncline}
-                />
-                <Input
-                  placeholder="Elevation (m)"
-                  value={elevation}
-                  onChange={setElevation}
-                />
-              </View>
-              <Button title="Upload" onPress={() => console.log("Upload")} />
-              <Button title="Reset" onPress={() => reset()} />
-              <StatusBar style="light" />
-            </ScrollView>
-          </KeyboardAvoidingView>
-        </TouchableWithoutFeedback>
-      </View>
-    </SafeAreaView>
-  );
+						<ScrollView
+							contentContainerStyle={styles.container}
+							scrollEnabled={false}
+						>
+							<View style={styles.inputContainer}>
+								<Input
+									placeholder="Title (e.g. My Morning Run)"
+									value={title}
+									onChange={setTitle}
+								/>
+								<Input
+									placeholder="Distance (km)"
+									value={distance}
+									onChange={setDistance}
+								/>
+								<Input
+									placeholder="Duration (hh:mm:ss)"
+									value={duration}
+									onChange={setDuration}
+								/>
+								<Input
+									placeholder="Date (dd/mm/yyyy)"
+									value={date}
+									onChange={setDate}
+								/>
+								<Input
+									placeholder="Time (hh:mm)"
+									value={time}
+									onChange={setTime}
+								/>
+								<Input
+									placeholder="Incline (%)"
+									value={incline}
+									onChange={setIncline}
+								/>
+								<Input
+									placeholder="Elevation (m)"
+									value={elevation}
+									onChange={setElevation}
+								/>
+							</View>
+							<Button title="Upload" onPress={() => console.log("Upload")} />
+							<Button title="Reset" onPress={() => reset()} />
+							<StatusBar style="light" />
+						</ScrollView>
+					</KeyboardAvoidingView>
+				</TouchableWithoutFeedback>
+			</View>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 30,
-  },
-  inputContainer: {
-    paddingBottom: 140,
-  },
-  button: {
-    padding: 10,
-  },
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingBottom: 120,
-  },
-  title: {
-    fontSize: 34,
-    color: "#FFF",
-    textAlign: "center",
-  },
-  text: {
-    fontSize: 20,
-    color: "#FFF",
-  },
+	container: {
+		paddingHorizontal: 30,
+	},
+	inputContainer: {
+		paddingBottom: 140,
+	},
+	button: {
+		padding: 10,
+	},
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		paddingBottom: 120,
+	},
+	title: {
+		fontSize: 34,
+		color: "#FFF",
+		textAlign: "center",
+	},
+	text: {
+		fontSize: 20,
+		color: "#FFF",
+	},
 });
 
 const Button = ({ onPress, title }) => {
-  return (
-    <Pressable style={styles.button} onPress={onPress}>
-      <Text
-        style={[
-          styles.text,
-          { textAlign: "center", textTransform: "uppercase" },
-        ]}
-      >
-        {title}
-      </Text>
-    </Pressable>
-  );
+	return (
+		<Pressable style={styles.button} onPress={onPress}>
+			<Text
+				style={[
+					styles.text,
+					{ textAlign: "center", textTransform: "uppercase" },
+				]}
+			>
+				{title}
+			</Text>
+		</Pressable>
+	);
 };
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,15 +2,14 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pbitzroejhdbkjocdyof.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBiaXR6cm9lamhkYmtqb2NkeW9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI1Mzc2ODksImV4cCI6MjAyODExMzY4OX0.-giPLe1CyWtl8yUQ7NHRzxbStlhn7SAskbdKtx0W0jg";
+const supabaseUrl = "https://vbrtofwoiyuftfjzvmhs.supabase.co/functions/v1/api";
+const supabasePublishableKey = "sb_publishable_hSx9UqfgcbSTl7rJd33l3Q_KLILi9JC";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    storage: AsyncStorage,
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-  },
+export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
+	auth: {
+		storage: AsyncStorage,
+		autoRefreshToken: true,
+		persistSession: true,
+		detectSessionInUrl: false,
+	},
 });
